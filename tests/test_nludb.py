04acb9d4df7ddab9452be65de9f01077ad8b1a21
Edward Benson <edward.benson@gmail.com>
2021-05-18 16:09:58 +0800

    All tests green after addition of background task handling.

--- tests/test_nludb.py
+++ tests/test_nludb.py
@@ -47,122 +47,122 @@ def test_connect():
     """Test basic connection"""
     nludb = _nludb()
 
-# def test_index_create():
-#     nludb = _nludb()
-#     name = _random_name()
+def test_index_create():
+    nludb = _nludb()
+    name = _random_name()
 
-#     # Should require name
-#     with pytest.raises(Exception):
-#         index = nludb.create_index(
-#             model=EmbeddingModels.QA
-#         )
+    # Should require name
+    with pytest.raises(Exception):
+        index = nludb.create_index(
+            model=EmbeddingModels.QA
+        )
 
-#     # Should require model
-#     with pytest.raises(Exception):
-#         index = nludb.create_index(
-#             name="Test Index"
-#         )
+    # Should require model
+    with pytest.raises(Exception):
+        index = nludb.create_index(
+            name="Test Index"
+        )
 
-#     index = nludb.create_index(
-#         name=name,
-#         model=EmbeddingModels.QA,
-#         upsert=True
-#     )
-#     assert index is not None
+    index = nludb.create_index(
+        name=name,
+        model=EmbeddingModels.QA,
+        upsert=True
+    )
+    assert index is not None
 
-#     # Duplicate creation should fail with upsert=False
-#     with pytest.raises(Exception, match=r".*already exists.*"):
-#         index = nludb.create_index(
-#             name="Test Index",
-#             model=EmbeddingModels.QA,
-#             upsert=False
-#         )
+    # Duplicate creation should fail with upsert=False
+    with pytest.raises(Exception, match=r".*already exists.*"):
+        index = nludb.create_index(
+            name="Test Index",
+            model=EmbeddingModels.QA,
+            upsert=False
+        )
 
-#     index.delete()
+    index.delete()
 
-# def test_basic_embeddings():
-#     nludb = _nludb()
-#     e1 = nludb.embed(["This is a test"], EmbeddingModels.QA)
-#     e1b = nludb.embed(["Banana"], EmbeddingModels.QA)
-#     assert(len(e1.embeddings) == 1)
-#     assert(len(e1.embeddings[0]) == 768)
+def test_basic_embeddings():
+    nludb = _nludb()
+    e1 = nludb.embed(["This is a test"], EmbeddingModels.QA)
+    e1b = nludb.embed(["Banana"], EmbeddingModels.QA)
+    assert(len(e1.embeddings) == 1)
+    assert(len(e1.embeddings[0]) == 768)
 
-#     e2 = nludb.embed(["This is a test"], EmbeddingModels.QA)
-#     assert(len(e2.embeddings) == 1)
-#     assert(len(e2.embeddings[0]) == 768)
+    e2 = nludb.embed(["This is a test"], EmbeddingModels.QA)
+    assert(len(e2.embeddings) == 1)
+    assert(len(e2.embeddings[0]) == 768)
 
-#     e3 = nludb.embed(["This is a test"], EmbeddingModels.SIMILARITY)
-#     e3b = nludb.embed(["Banana"], EmbeddingModels.SIMILARITY)
-#     assert(len(e3.embeddings) == 1)
-#     assert(len(e3.embeddings[0]) == 768)
+    e3 = nludb.embed(["This is a test"], EmbeddingModels.SIMILARITY)
+    e3b = nludb.embed(["Banana"], EmbeddingModels.SIMILARITY)
+    assert(len(e3.embeddings) == 1)
+    assert(len(e3.embeddings[0]) == 768)
 
-#     e4 = nludb.embed(["This is a test"], EmbeddingModels.SIMILARITY)
-#     assert(len(e4.embeddings) == 1)
-#     assert(len(e4.embeddings[0]) == 768)
+    e4 = nludb.embed(["This is a test"], EmbeddingModels.SIMILARITY)
+    assert(len(e4.embeddings) == 1)
+    assert(len(e4.embeddings[0]) == 768)
 
-#     e5 = nludb.embed(["This is a test"], EmbeddingModels.PARAPHRASE)
-#     e5b = nludb.embed(["Banana"], EmbeddingModels.PARAPHRASE)
-#     assert(len(e5.embeddings) == 1)
-#     assert(len(e5.embeddings[0]) == 768)
+    e5 = nludb.embed(["This is a test"], EmbeddingModels.PARAPHRASE)
+    e5b = nludb.embed(["Banana"], EmbeddingModels.PARAPHRASE)
+    assert(len(e5.embeddings) == 1)
+    assert(len(e5.embeddings[0]) == 768)
 
-#     e6 = nludb.embed(["This is a test"], EmbeddingModels.PARAPHRASE)
-#     assert(len(e6.embeddings) == 1)
-#     assert(len(e6.embeddings[0]) == 768)
+    e6 = nludb.embed(["This is a test"], EmbeddingModels.PARAPHRASE)
+    assert(len(e6.embeddings) == 1)
+    assert(len(e6.embeddings[0]) == 768)
 
-#     assert(e1.embeddings[0] == e2.embeddings[0])
-#     assert(e3.embeddings[0] == e4.embeddings[0])
-#     assert(e5.embeddings[0] == e6.embeddings[0])
+    assert(e1.embeddings[0] == e2.embeddings[0])
+    assert(e3.embeddings[0] == e4.embeddings[0])
+    assert(e5.embeddings[0] == e6.embeddings[0])
 
-#     assert(e1.embeddings[0] != e1b.embeddings[0])
-#     assert(e3.embeddings[0] != e3b.embeddings[0])
-#     assert(e5.embeddings[0] != e5b.embeddings[0])
+    assert(e1.embeddings[0] != e1b.embeddings[0])
+    assert(e3.embeddings[0] != e3b.embeddings[0])
+    assert(e5.embeddings[0] != e5b.embeddings[0])
 
-#     assert(e1.embeddings[0] != e4.embeddings[0])
-#     assert(e1.embeddings[0] != e6.embeddings[0])
-#     assert(e3b.embeddings[0] != e5b.embeddings[0])
-#     assert(e4.embeddings[0] != e6.embeddings[0])
+    assert(e1.embeddings[0] != e4.embeddings[0])
+    assert(e1.embeddings[0] != e6.embeddings[0])
+    assert(e3b.embeddings[0] != e5b.embeddings[0])
+    assert(e4.embeddings[0] != e6.embeddings[0])
 
-# def test_basic_embedding_search():
-#     nludb = _nludb()
-#     docs = [
-#         "Armadillo shells are bulletproof.",
-#         "Dolphins sleep with one eye open.",
-#         "Alfred Hitchcock was frightened of eggs.",
-#         "Jonathan can help you with new employee onboarding",
-#         "The code for the New York office is 1234",
-#     ]
-#     query = "Who should I talk to about new employee setup?"
-#     results = nludb.embed_and_search(query, docs, EmbeddingModels.QA)
-#     assert(len(results.hits) == 1)
-#     assert(results.hits[0].value == "Jonathan can help you with new employee onboarding")
+def test_basic_embedding_search():
+    nludb = _nludb()
+    docs = [
+        "Armadillo shells are bulletproof.",
+        "Dolphins sleep with one eye open.",
+        "Alfred Hitchcock was frightened of eggs.",
+        "Jonathan can help you with new employee onboarding",
+        "The code for the New York office is 1234",
+    ]
+    query = "Who should I talk to about new employee setup?"
+    results = nludb.embed_and_search(query, docs, EmbeddingModels.QA)
+    assert(len(results.hits) == 1)
+    assert(results.hits[0].value == "Jonathan can help you with new employee onboarding")
 
 
-# def test_index_delete():
-#     nludb = _nludb()
-#     name = _random_name()
-#     index = nludb.create_index(
-#         name=name,
-#         model=EmbeddingModels.QA,
-#         upsert=True
-#     )
-#     assert(index.id is not None)
+def test_index_delete():
+    nludb = _nludb()
+    name = _random_name()
+    index = nludb.create_index(
+        name=name,
+        model=EmbeddingModels.QA,
+        upsert=True
+    )
+    assert(index.id is not None)
 
-#     index2 = nludb.create_index(
-#         name=name,
-#         model=EmbeddingModels.QA,
-#         upsert=True
-#     )
-#     assert(index.id == index2.id)
+    index2 = nludb.create_index(
+        name=name,
+        model=EmbeddingModels.QA,
+        upsert=True
+    )
+    assert(index.id == index2.id)
     
-#     index.delete()
+    index.delete()
 
-#     index3 = nludb.create_index(
-#         name=name,
-#         model=EmbeddingModels.QA,
-#         upsert=True
-#     )
-#     assert(index.id != index3.id)
-#     index3.delete()
+    index3 = nludb.create_index(
+        name=name,
+        model=EmbeddingModels.QA,
+        upsert=True
+    )
+    assert(index.id != index3.id)
+    index3.delete()
 
 def test_embed_task():
     nludb = _nludb()
@@ -190,60 +190,63 @@ def test_embed_task():
         assert (task.taskStatus == NludbTaskStatus.succeeded)
 
 
-# def test_index_usage():
-#     nludb = _nludb()
-#     name = _random_name()
+def test_index_usage():
+    nludb = _nludb()
+    name = _random_name()
 
-#     with _random_index(nludb) as index:
-#         # Test for supressed reindexing
-#         A1 = "Ted can eat an entire block of cheese."
-#         Q1 = "Who can eat the most cheese"
-#         insert_results = index.insert(A1, reindex=False )
-#         search_results = index.search(Q1)
+    with _random_index(nludb) as index:
+        # Test for supressed reindexing
+        A1 = "Ted can eat an entire block of cheese."
+        Q1 = "Who can eat the most cheese"
+        insert_results = index.insert(A1, reindex=False )
+        search_results = index.search(Q1)
 
-#         # Now embed
-#         task = index.embed()
-#         search_results = index.search(Q1)
-#         assert(len(search_results.hits) == 1)
-#         assert(search_results.hits[0].value == A1)
+        # Now embed
+        task = index.embed()
+        task._run_development_mode()
+        task.wait()
+        
+        search_results = index.search(Q1)
+        assert(len(search_results.hits) == 1)
+        assert(search_results.hits[0].value == A1)
 
-#         # Associate metadata
-#         A2 = "Armadillo shells are bulletproof."
-#         Q2 = "What is something interesting about Armadillos?"
-#         A2id = "A2id"
-#         A2type = "A2type"
-#         A2metadata = dict(
-#             id=A2id, 
-#             idid="{}{}".format(A2id, A2id),
-#             boolVal=True,
-#             intVal=123,
-#             floatVal=1.2
-#         )
+        # Associate metadata
+        A2 = "Armadillo shells are bulletproof."
+        Q2 = "What is something interesting about Armadillos?"
+        A2id = "A2id"
+        A2type = "A2type"
+        A2metadata = dict(
+            id=A2id, 
+            idid="{}{}".format(A2id, A2id),
+            boolVal=True,
+            intVal=123,
+            floatVal=1.2
+        )
 
-#         insert_results2 = index.insert(
-#             A2,
-#             externalId=A2id,
-#             externalType=A2type,
-#             metadata=A2metadata
-#         )
-#         search_results2 = index.search(Q2)
-#         assert(len(search_results2.hits) == 1)
-#         assert(search_results2.hits[0].value == A2)
-#         assert(search_results2.hits[0].externalId == None)
-#         assert(search_results2.hits[0].externalType == None)
-#         assert(search_results2.hits[0].metadata == None)
+        insert_results2 = index.insert(
+            A2,
+            externalId=A2id,
+            externalType=A2type,
+            metadata=A2metadata
+        )
+        search_results2 = index.search(Q2)
+        assert(len(search_results2.hits) == 1)
+        assert(search_results2.hits[0].value == A2)
+        assert(search_results2.hits[0].externalId == None)
+        assert(search_results2.hits[0].externalType == None)
+        assert(search_results2.hits[0].metadata == None)
 
-#         search_results2 = index.search(Q2, includeMetadata=True)
-#         assert(len(search_results2.hits) == 1)
-#         assert(search_results2.hits[0].value == A2)
-#         assert(search_results2.hits[0].externalId == A2id)
-#         assert(search_results2.hits[0].externalType == A2type)
-#         assert(search_results2.hits[0].metadata == A2metadata)
-#         # Because I don't know pytest enough to fullly trust the dict comparison..
-#         assert(search_results2.hits[0].metadata["id"] == A2id)
-#         assert(search_results2.hits[0].metadata["idid"] == "{}{}".format(A2id, A2id))
+        search_results2 = index.search(Q2, includeMetadata=True)
+        assert(len(search_results2.hits) == 1)
+        assert(search_results2.hits[0].value == A2)
+        assert(search_results2.hits[0].externalId == A2id)
+        assert(search_results2.hits[0].externalType == A2type)
+        assert(search_results2.hits[0].metadata == A2metadata)
+        # Because I don't know pytest enough to fullly trust the dict comparison..
+        assert(search_results2.hits[0].metadata["id"] == A2id)
+        assert(search_results2.hits[0].metadata["idid"] == "{}{}".format(A2id, A2id))
 
-#         search_results2 = index.search(Q2, k=10)
-#         assert(len(search_results2.hits) == 2)
-#         assert(search_results2.hits[0].value == A2)
-#         assert(search_results2.hits[1].value == A1)
+        search_results2 = index.search(Q2, k=10)
+        assert(len(search_results2.hits) == 2)
+        assert(search_results2.hits[0].value == A2)
+        assert(search_results2.hits[1].value == A1)
