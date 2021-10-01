a74d4f396042872298921ef0135762e308c6104e
Edward Benson <edward.benson@gmail.com>
2021-10-01 09:24:53 -0400

    Uncomment snapshot test and update to new response obj

--- tests/test_snapshots.py
+++ tests/test_snapshots.py
@@ -45,53 +45,53 @@ def _snapshot(index, windowSize=None):
     task.wait()
     task.check()
 
-# def test_snapshot_create():
-#   nludb = _nludb()
+def test_snapshot_create():
+  nludb = _nludb()
 
-#   name = _random_name()
-#   index = nludb.create_index(
-#       name=name,
-#       model=qa_model(),
-#       upsert=True
-#   )
+  name = _random_name()
+  index = nludb.create_index(
+      name=name,
+      model=qa_model(),
+      upsert=True
+  )
 
-#   _insert(index, ["Oranges are orange."])
-#   search_results = index.search("What color are oranges?", includeMetadata=True)
-#   assert(len(search_results.hits) == 1)
-#   assert(search_results.hits[0].indexSource == "index")
-#   assert(search_results.hits[0].value == "Oranges are orange.")
-#   assert(search_results.hits[0].externalId == "TestId")
-#   assert(search_results.hits[0].externalType == "TestType")
-#   assert(len(search_results.hits[0].metadata)  == 3)
+  _insert(index, ["Oranges are orange."])
+  search_results = index.search("What color are oranges?", includeMetadata=True)
+  assert(len(search_results.data.hits) == 1)
+  assert(search_results.data.hits[0].indexSource == "index")
+  assert(search_results.data.hits[0].value == "Oranges are orange.")
+  assert(search_results.data.hits[0].externalId == "TestId")
+  assert(search_results.data.hits[0].externalType == "TestType")
+  assert(len(search_results.data.hits[0].metadata)  == 3)
 
-#   _snapshot(index)
-#   search_results = index.search("What color are oranges?", includeMetadata=True)
-#   assert(len(search_results.hits) == 1)
-#   assert(search_results.hits[0].indexSource == "snapshot")
-#   assert(search_results.hits[0].value == "Oranges are orange.")
-#   assert(search_results.hits[0].externalId == "TestId")
-#   assert(search_results.hits[0].externalType == "TestType")
-#   assert(len(search_results.hits[0].metadata)  == 3)
+  _snapshot(index)
+  search_results = index.search("What color are oranges?", includeMetadata=True)
+  assert(len(search_results.data.hits) == 1)
+  assert(search_results.data.hits[0].indexSource == "snapshot")
+  assert(search_results.data.hits[0].value == "Oranges are orange.")
+  assert(search_results.data.hits[0].externalId == "TestId")
+  assert(search_results.data.hits[0].externalType == "TestType")
+  assert(len(search_results.data.hits[0].metadata)  == 3)
 
-#   _insert(index, ["Apples are red."])
-#   search_results = index.search("What color are apples?", includeMetadata=True)
-#   assert(len(search_results.hits) == 1)
-#   assert(search_results.hits[0].indexSource == "index")
-#   assert(search_results.hits[0].value == "Apples are red.")
-#   assert(search_results.hits[0].externalId == "TestId")
-#   assert(search_results.hits[0].externalType == "TestType")
-#   assert(len(search_results.hits[0].metadata)  == 3)
+  _insert(index, ["Apples are red."])
+  search_results = index.search("What color are apples?", includeMetadata=True)
+  assert(len(search_results.data.hits) == 1)
+  assert(search_results.data.hits[0].indexSource == "index")
+  assert(search_results.data.hits[0].value == "Apples are red.")
+  assert(search_results.data.hits[0].externalId == "TestId")
+  assert(search_results.data.hits[0].externalType == "TestType")
+  assert(len(search_results.data.hits[0].metadata)  == 3)
 
-#   _snapshot(index)
-#   search_results = index.search("What color are apples?", includeMetadata=True)
-#   assert(len(search_results.hits) == 1)
-#   assert(search_results.hits[0].indexSource == "snapshot")
-#   assert(search_results.hits[0].value == "Apples are red.")
-#   assert(search_results.hits[0].externalId == "TestId")
-#   assert(search_results.hits[0].externalType == "TestType")
-#   assert(len(search_results.hits[0].metadata)  == 3)
+  _snapshot(index)
+  search_results = index.search("What color are apples?", includeMetadata=True)
+  assert(len(search_results.data.hits) == 1)
+  assert(search_results.data.hits[0].indexSource == "snapshot")
+  assert(search_results.data.hits[0].value == "Apples are red.")
+  assert(search_results.data.hits[0].externalId == "TestId")
+  assert(search_results.data.hits[0].externalType == "TestType")
+  assert(len(search_results.data.hits[0].metadata)  == 3)
 
-#   index.delete()
+  index.delete()
 
 def test_snapshot_window():
   nludb = _nludb()
@@ -111,20 +111,20 @@ def test_snapshot_window():
   _insert(index, sentences)
 
   search_results = index.search(SENT, includeMetadata=True).data
-  assert(len(search_results.hits) == 1)
-  assert(search_results.hits[0].indexSource == "index")
-  assert(search_results.hits[0].value == "Orange number 13 is as good as the last")
-  assert(search_results.hits[0].externalId == "TestId")
-  assert(search_results.hits[0].externalType == "TestType")
-  assert(len(search_results.hits[0].metadata)  == 3)
+  assert(len(search_results.data.hits) == 1)
+  assert(search_results.data.hits[0].indexSource == "index")
+  assert(search_results.data.hits[0].value == "Orange number 13 is as good as the last")
+  assert(search_results.data.hits[0].externalId == "TestId")
+  assert(search_results.data.hits[0].externalType == "TestType")
+  assert(len(search_results.data.hits[0].metadata)  == 3)
 
   _snapshot(index, windowSize=2)
   search_results = index.search(SENT, includeMetadata=True).data
-  assert(len(search_results.hits) == 1)
-  assert(search_results.hits[0].indexSource == "snapshot")
-  assert(search_results.hits[0].value == "Orange number 13 is as good as the last")
-  assert(search_results.hits[0].externalId == "TestId")
-  assert(search_results.hits[0].externalType == "TestType")
-  assert(len(search_results.hits[0].metadata)  == 3)
+  assert(len(search_results.data.hits) == 1)
+  assert(search_results.data.hits[0].indexSource == "snapshot")
+  assert(search_results.data.hits[0].value == "Orange number 13 is as good as the last")
+  assert(search_results.data.hits[0].externalId == "TestId")
+  assert(search_results.data.hits[0].externalType == "TestType")
+  assert(len(search_results.data.hits[0].metadata)  == 3)
 
   index.delete()
