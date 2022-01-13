ab2bc07e89a41b41525372e7fb7106778a831aad
Edward Benson <edward.benson@gmail.com>
2022-01-12 22:44:45 -0500

    file_parse tests green again

--- tests/test_file_parse.py
+++ tests/test_file_parse.py
@@ -1,3 +1,4 @@
+from nludb.types.base import TaskStatus
 from nludb.types.parsing_models import ParsingModels
 import pytest
 from os import path
@@ -14,48 +15,55 @@ __license__ = "MIT"
 def test_file_parse():
   nludb = _nludb()
   name_a = "{}.mkd".format(_random_name())
-  T = "This is the title"
-  P1_1 = "This is the first sentence of paragraph 1."
-  P1_2 = "This is the second sentence of paragraph 1."
-  P2_1 = "This is the first sentence of paragraph 2."
-  P2_2 = "This is the second sentence of paragraph 2."
+  T = "A Poem"
+  P1_1 = "Roses are red."
+  P1_2 = "Violets are blue."
+  P2_1 = "Sugar is sweet, and I love you."
 
-  CONTENT = "# {}\n\n{} {}\n\n{} {}".format(T, P1_1, P1_2, P2_1, P2_2)
+  CONTENT = "# {}\n\n{} {}\n\n{}".format(T, P1_1, P1_2, P2_1)
 
   a = nludb.upload(
     name=name_a,
     content=CONTENT,
-    format=FileFormats.MKD
-  )
+    mimeType=FileFormats.MKD
+  ).data
   assert(a.id is not None)
   assert(a.name == name_a)
-  assert(a.format == FileFormats.MKD)
+  assert(a.mimeType == FileFormats.MKD)
 
   a.convert().wait()
 
-  raw = a.raw().data
-  assert(raw == CONTENT)
+  raw = a.raw()
+  assert(raw.decode('utf-8') == CONTENT)
 
   q1 = a.query(blockType=BlockTypes.H1).data
   assert(len(q1.blocks) == 1)
   assert(q1.blocks[0].type == BlockTypes.H1)
-  assert(q1.blocks[0].value == T)
+  assert(q1.blocks[0].text == T)
 
   q2 = a.query(blockType=BlockTypes.Paragraph).data
   assert(len(q2.blocks) == 2)
   assert(q2.blocks[0].type == BlockTypes.Paragraph)
-  assert(q2.blocks[0].value == "{} {}".format(P1_1, P1_2))
+  assert(q2.blocks[0].text == "{} {}".format(P1_1, P1_2))
 
   # The sentences aren't yet parsed out!
   q2 = a.query(blockType=BlockTypes.Sentence).data
   assert(len(q2.blocks) == 0)
 
   # Now we parse
-  a.parse(model=ParsingModels.EN_DEFAULT).wait()
+  task = a.parse(model=ParsingModels.UNIT_TEST)
+  assert(task.error is None)
+  assert(task.task is not None)
+  assert(task.task.taskStatus == TaskStatus.waiting)
+
+  task.wait()
+  assert(task.error is None)
+  assert(task.task is not None)
+  assert(task.task.taskStatus == TaskStatus.succeeded)
 
   # Now the sentences should be parsed!
   q2 = a.query(blockType=BlockTypes.Sentence).data
-  assert(len(q2.blocks) == 5) # The 5th is inside the header!
+  assert(len(q2.blocks) == 2) # The 5th is inside the header!
 
   a.clear()
 
