9903dfd5a4092f95656c5eb64a2facab81619e07
Enias Cailliau <hello@enias.ai>
2022-05-06 21:19:10 +0200

    Fix tests (#45)
    Remove broken test and add Pydantic dependency
    
    Co-authored-by: Enias Cailliau <enias.oryx@gmail.com>

--- tests/plugin/test_tsv_blockifier.py
+++
@@ -1,9 +0,0 @@
-__copyright__ = "Steamship"
-__license__ = "MIT"
-
-from ..demo_apps.plugins.tsv_blockifier import TsvBlockifier
-
-print("hello")
-
-def test_importer():
-    importer = TsvBlockifier()
