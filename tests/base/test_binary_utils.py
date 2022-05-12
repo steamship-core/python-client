3477cb92223baa9b378915b2855e554c73c8c1fa
Ted Benson <ted@steamship.com>
2022-05-11 23:42:40 -0400

    Additional test

--- tests/base/test_binary_utils.py
+++ tests/base/test_binary_utils.py
@@ -1,3 +1,4 @@
+import base64
 import dataclasses
 import json
 
@@ -5,6 +6,7 @@ __copyright__ = "Steamship"
 __license__ = "MIT"
 
 from steamship.base.binary_utils import flexi_create
+from steamship.plugin.outputs.raw_data_plugin_output import RawDataPluginOutput
 
 
 def test_dump_string():
@@ -35,3 +37,27 @@ def test_dump_json():
 
     person2 = Person2(name="Ted")
     assert (json.dumps(flexi_create(json=person2)[0]) == '{"takes": "precedence"}')
+
+
+def _base64_decode(base64_message: str) -> str:
+    base64_bytes = base64_message.encode('ascii')
+    message_bytes = base64.b64decode(base64_bytes)
+    return message_bytes.decode('utf8')
+
+
+def test_dump_raw_data():
+    """This makes use of flexi_create. We want to ensure it is properly base64 encoding the JSON representation of
+    objects given to it."""
+
+    obj = RawDataPluginOutput(json={'hi': 'there'})
+    json_str = _base64_decode(obj.data)
+    assert(json_str == '{"hi": "there"}')
+
+    @dataclasses.dataclass
+    class Person:
+        name: str
+
+    person = Person(name="Ted")
+    obj2 = RawDataPluginOutput(json=person)
+    json_str2 = _base64_decode(obj2.data)
+    assert (json_str2 == '{"name": "Ted"}')
