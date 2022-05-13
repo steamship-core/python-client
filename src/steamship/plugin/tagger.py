337b4031ff497e8d6aef65ea0899611db69a9830
Ted Benson <ted@steamship.com>
2022-05-13 11:03:27 -0400

    WIP

--- src/steamship/plugin/tagger.py
+++ src/steamship/plugin/tagger.py
@@ -1,6 +1,7 @@
 from abc import ABC
 from typing import Any
 
+from steamship.app import Response, post
 from steamship.base import Client
 from steamship.plugin.inputs.block_and_tag_plugin_input import BlockAndTagPluginInput
 from steamship.plugin.outputs.block_and_tag_plugin_output import BlockAndTagPluginOutput
@@ -21,3 +22,9 @@ class Tagger(PluginService[BlockAndTagPluginInput, BlockAndTagPluginOutput], ABC
         cls, d: Any, client: Client = None
     ) -> PluginRequest[BlockAndTagPluginInput]:
         return BlockAndTagPluginInput.from_dict(d, client=client)
+
+
+    @post("getTrainingParameters")
+    def _get_training_parameters_endpoint(self, **kwargs) -> dict:
+        parse_request = Tagger.parse_request(request=kwargs)
+        return self.run(parse_request)
