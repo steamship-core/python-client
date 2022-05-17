2580ee2691b57d49923d571e8c2962707b0339a5
Enias Cailliau <enias.oryx@gmail.com>
2022-05-17 20:17:58 +0200

    Put back corpus importer

---
+++ src/steamship/client/operations/corpus_importer.py
@@ -0,0 +1,74 @@
+from dataclasses import dataclass
+from typing import Any, List
+
+from steamship import File
+from steamship.base import Client
+
+
+@dataclass
+class CorpusImportRequest:  # TODO (enias): Depricate
+    # The Corpus Identifiers
+    client: Client = None
+    id: str = None
+    handle: str = None
+    type: str = "corpus"
+
+    # Data for the plugin
+    value: str = None
+    data: str = None
+    url: str = None
+    pluginInstance: str = None
+    fileImporterPluginInstance: str = None
+
+    @staticmethod
+    def from_dict(d: Any, client: Client = None) -> "CorpusImportRequest":
+        # noinspection PyArgumentEqualDefault
+        return CorpusImportRequest(
+            client=client,
+            id=d.get("id"),
+            handle=d.get("handle"),
+            type="corpus",
+            value=d.get("value"),
+            data=d.get("data"),
+            url=d.get("url"),
+            pluginInstance=d.get("pluginInstance"),
+            fileImporterPluginInstance=d.get("fileImporterPluginInstance"),
+        )
+
+    def to_dict(self) -> dict:
+        return dict(
+            id=self.id,
+            handle=self.handle,
+            type=self.type,
+            value=self.value,
+            data=self.data,
+            url=self.url,
+            pluginInstance=self.pluginInstance,
+            fileImporterPluginInstance=self.fileImporterPluginInstance,
+        )
+
+
+@dataclass
+class CorpusImportResponse:
+    client: Client = None
+    fileImportRequests: List[File.CreateRequest] = None
+
+    def __init__(
+        self,
+        client: Client = None,
+        file_import_requests: List[File.CreateRequest] = None,
+    ):
+        self.client = client
+        self.fileImportRequests = file_import_requests
+
+    @staticmethod
+    def from_dict(d: Any, client: Client = None) -> "CorpusImportResponse":
+        return CorpusImportResponse(
+            client=client,
+            file_import_requests=[
+                File.CreateRequest.from_dict(req) for req in d.get("fileImportRequests", [])
+            ],
+        )
+
+    def to_dict(self) -> dict:
+        return dict(fileImportRequests=self.fileImportRequests)
---
+++ tests/plugin/test_e2e_corpus_importer.py
@@ -0,0 +1,43 @@
+from steamship import File, PluginInstance
+from tests.demo_apps.plugins.importers.plugin_file_importer import TEST_DOC
+
+from .. import APPS_PATH
+
+__copyright__ = "Steamship"
+__license__ = "MIT"
+
+from ..utils.client import get_steamship_client
+from ..utils.deployables import deploy_plugin
+from ..utils.random import random_corpus
+
+
+def test_e2e_corpus_importer():
+    client = get_steamship_client()
+    corpus_importer_path = APPS_PATH / "plugins" / "importers" / "plugin_corpus_importer.py"
+
+    test_file_importer_instance = PluginInstance.create(
+        client, plugin_handle="test-fileImporter-valueOrData", upsert=True
+    ).data
+    with deploy_plugin(client, corpus_importer_path, "corpusImporter") as (
+        plugin,
+        version,
+        instance,
+    ):
+        # with PluginVersion.(client, "corpusImporter", "do_import", app, instance) as plugin:
+        with random_corpus(client) as corpus:
+            resp = corpus.do_import(
+                plugin_instance=instance.handle,
+                file_importer_plugin_instance=test_file_importer_instance.handle,
+                value="dummy-value",
+            )
+            resp.wait()
+
+            # We should now have two files!
+            files = File.list(client, corpus_id=corpus.id).data
+            assert files.files is not None
+            assert len(files.files) == 2
+
+            for file in files.files:
+                data = file.raw().data
+                assert data.decode("utf-8") == TEST_DOC
+                file.delete()
