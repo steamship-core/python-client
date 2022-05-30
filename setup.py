c59f42253169cad6ba8dd491c9bb1014f30b3b09
Ted Benson <ted@steamship.com>
2022-05-30 18:10:19 -0400

    Try semver

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 if __name__ == "__main__":
     try:
-        setup(use_scm_version={"version_scheme": "no-guess-dev"})
+        setup(use_scm_version={"version_scheme": "release-branch-semver"})
     except Exception as e:
         print(
             "\n\nAn error occurred while building the project, "
