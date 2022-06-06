59fba4303bbae2f7eb2d269272de05df772d4943
Dave Kolas <4643013+dkolas@users.noreply.github.com>
2022-06-06 13:06:23 -0500

    Allow explicit False values in Plugin Config (#93)

--- src/steamship/plugin/config.py
+++ src/steamship/plugin/config.py
@@ -5,5 +5,5 @@ class Config(BaseModel):
     """Base class for a Steamship Plugin's configuration object."""
 
     def __init__(self, **kwargs):
-        kwargs = {k: v for k, v in kwargs.items() if v}
+        kwargs = {k: v for k, v in kwargs.items() if v is not None}
         super().__init__(**kwargs)
