97aaf6e4be833bfc8dde7900f9442c890bbaa328
Enias Cailliau <hello@enias.ai>
2022-06-13 11:14:52 +0200

    Raise exception when data is accessed on an incomplete asynchronous request (#95)

--- tests/tests/base/test_response.py
+++ tests/tests/base/test_response.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import pytest
 
 from steamship import SteamshipError
-from steamship.base import Response, Task
+from steamship.base import Response, Task, TaskState
 
 TEST_DATA = {"test": "test"}
 
@@ -17,13 +17,32 @@ def test_no_data_response():
 
 def test_async_response():
     response = Response[dict](task=Task())
-    assert response.data is None
-    response.update(Response[dict](task=Task(), data_=TEST_DATA))
+    with pytest.raises(SteamshipError):
+        _ = response.data
+
+    response.update(Response[dict](task=Task(state=TaskState.succeeded), data_=TEST_DATA))
     assert response.data is not None
-    assert response.data["test"] == "test"
+    assert response.data == TEST_DATA
+
+    for ongoing_state in (TaskState.running, TaskState.waiting):
+        response = Response[dict](task=Task(state=ongoing_state))
+        with pytest.raises(SteamshipError):
+            _ = response.data
+
+        response.update(Response[dict](task=Task(state=ongoing_state), data_=TEST_DATA))
+        assert response.data is not None
+        assert response.data == TEST_DATA
+
+    response = Response[dict](task=Task(state=TaskState.failed))
+    with pytest.raises(SteamshipError):
+        _ = response.data
+
+    response.update(Response[dict](task=Task(state=TaskState.failed), data_=TEST_DATA))
+    with pytest.raises(SteamshipError):
+        _ = response.data
 
 
 def test_data_response():
     response = Response[dict](data_=TEST_DATA)
     assert response.data is not None
-    assert response.data["test"] == "test"
+    assert response.data == TEST_DATA
