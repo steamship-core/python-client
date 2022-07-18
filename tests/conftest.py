b0adbb778c811587b18dc4076ac29bdecc31dfad
Ted Benson <ted@steamship.com>
2022-07-18 16:11:44 -0400

    Make sure to append the pah before importing from steamship_tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ Pytest reads this file in before doing any work. Read more about conftest.py und
 import sys
 from pathlib import Path
 
-from steamship_tests.utils.fixtures import app_handler, client  # noqa: F401
-
 # Make sure `steamship_tests` is on the PYTHONPATH. Otherwise cross-test imports (e.g. to util libraries) will fail.
 sys.path.append(str(Path(__file__).parent.absolute()))
+
+from steamship_tests.utils.fixtures import app_handler, client  # noqa: F401, E402
