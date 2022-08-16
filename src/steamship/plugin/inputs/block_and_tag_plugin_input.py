f44d016b69b51bb9bb646a7f870d0dfa59d24c62
Ted Benson <ted@steamship.com>
2022-08-16 16:00:45 -0400

    Update

--- src/steamship/plugin/inputs/block_and_tag_plugin_input.py
+++ src/steamship/plugin/inputs/block_and_tag_plugin_input.py
@@ -2,7 +2,22 @@ from __future__ import annotations
 
 from steamship import File
 from steamship.base.configuration import CamelModel
+from steamship.base.error import SteamshipError
+from steamship.utils.signed_urls import url_to_json
 
 
 class BlockAndTagPluginInput(CamelModel):
     file: File = None
+
+    def __init__(self, **kwargs):
+        if url := kwargs.get("url"):
+            # If `url` was provided, we assume that some or all of the new object's parameterization exists
+            # at that location, encoded as JSON. We fetch it, parse as JSON, and fold into the kwarg dict.
+            result = url_to_json(url)
+            if type(result) != dict:
+                raise SteamshipError(
+                    message=f"BlockAndTagPluginInput received a URL that resolved to {type(result)}. Needed a `dict`"
+                )
+            kwargs.update(result)
+
+        super().__init__(**kwargs)
