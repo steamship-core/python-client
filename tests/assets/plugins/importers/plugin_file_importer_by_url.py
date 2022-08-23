78450cf6642ee66e86d357bf72a3309e6572f0e0
Dave Kolas <4643013+dkolas@users.noreply.github.com>
2022-08-23 07:54:26 -0500

    Automatically upload results into S3 if greater than 4mb (#131)

--- tests/assets/plugins/importers/plugin_file_importer_by_url.py
+++ tests/assets/plugins/importers/plugin_file_importer_by_url.py
@@ -1,16 +1,12 @@
-import logging
-import uuid
 from typing import Type
 
 from steamship import MimeTypes
 from steamship.app import Response, create_handler
-from steamship.data.space import SignedUrl
 from steamship.plugin.config import Config
 from steamship.plugin.file_importer import FileImporter
 from steamship.plugin.inputs.file_import_plugin_input import FileImportPluginInput
 from steamship.plugin.outputs.raw_data_plugin_output import RawDataPluginOutput
 from steamship.plugin.service import PluginRequest
-from steamship.utils.signed_urls import upload_to_signed_url
 
 # Note: this aligns with the same document in the internal Engine test.
 HANDLE = "test-importer-plugin-v1"
@@ -29,23 +25,8 @@ class TestFileImporterPlugin(FileImporter):
         return self.EmptyConfig
 
     def run(self, request: PluginRequest[FileImportPluginInput]) -> Response[RawDataPluginOutput]:
-        filepath = str(uuid.uuid4())
-        response = self.client.get_space().create_signed_url(
-            SignedUrl.Request(
-                bucket=SignedUrl.Bucket.PLUGIN_DATA,
-                filepath=filepath,
-                operation=SignedUrl.Operation.WRITE,
-            )
-        )
-        signed_url = response.data.signed_url
-        logging.info(f"Got signed url for writing: {signed_url}")
-        bytes = TEST_DOC.encode("utf-8")
-        upload_to_signed_url(signed_url, bytes)
-        return Response(
-            data=RawDataPluginOutput(
-                bucket=SignedUrl.Bucket.PLUGIN_DATA, filepath=filepath, mime_type=MimeTypes.MKD
-            )
-        )
+        bytes = (100000 * TEST_DOC).encode("utf-8")
+        return Response(data=RawDataPluginOutput(_bytes=bytes, mime_type=MimeTypes.MKD))
 
 
 handler = create_handler(TestFileImporterPlugin)
