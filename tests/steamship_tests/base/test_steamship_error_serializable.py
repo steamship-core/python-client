376ba20de00b099447b08ab4d0e60ff98a82bd99
dkolas <4643013+dkolas@users.noreply.github.com>
2022-08-30 11:45:46 -0400

    Fix tests again

--- tests/steamship_tests/base/test_steamship_error_serializable.py
+++ tests/steamship_tests/base/test_steamship_error_serializable.py
@@ -9,7 +9,7 @@ def test_serialize_steamship_error():
     serialized = msgpack.packb(error, default=encode_exception)
     assert (
         serialized
-        == b"\x85\xa7message\xbbOh noes! An error happened!\xafinternalMessage\xc0\xaasuggestion\xc0\xa4code\xc0\xa5error\xc0"
+        == b'\xd9t{"message": "Oh noes! An error happened!", "internalMessage": null, "suggestion": null, "code": null, "error": null}'
     )
 
 
@@ -19,14 +19,11 @@ def test_serialize_steamship_error_with_nested_error():
     serialized = msgpack.packb(error, default=encode_exception)
     assert (
         serialized
-        == b"\x85\xa7message\xbbOh noes! An error happened!\xafinternalMessage\xc0\xaasuggestion\xc0\xa4code\xc0\xa5error\xafA nested error!"
+        == b'\xd9\x81{"message": "Oh noes! An error happened!", "internalMessage": null, "suggestion": null, "code": null, "error": "A nested error!"}'
     )
 
 
 def test_serialize_non_steamship_error():
     error = AttributeError("An attribute error!")
     serialized = msgpack.packb(error, default=encode_exception)
-    assert (
-        serialized
-        == b"\x82\xafexception_class\xaeAttributeError\xa4args\x91\xb3An attribute error!"
-    )
+    assert serialized == b"\xd9?exception_class: AttributeError, args: ('An attribute error!',)"
