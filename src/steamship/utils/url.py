0d41c4195a7af180ff8a9bef2861008c89d671f9
dkolas <4643013+dkolas@users.noreply.github.com>
2022-11-22 11:59:36 -0500

    add  localstack as a signed url swap

--- src/steamship/utils/url.py
+++ src/steamship/utils/url.py
@@ -21,7 +21,7 @@ def apply_localstack_url_fix(url: Optional[str]) -> Optional[str]:
     logging.debug(f"URL {url}")
     localstack_hostname = environ.get("LOCALSTACK_HOSTNAME")
     if url and localstack_hostname is not None and localstack_hostname != "localhost":
-        for host in ["127.0.0.1", "host.docker.internal"]:
+        for host in ["127.0.0.1", "host.docker.internal", "localstack"]:
             url = url.replace(host, localstack_hostname)
             logging.info(f"Replacing domain {host} in {url} with {localstack_hostname}")
     return url
