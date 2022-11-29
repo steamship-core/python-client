0f0f5fa14b32bb4c1ae586f8e967325d7634aca0
Ted Benson <edward.benson@gmail.com>
2022-11-28 20:24:53 -0500

    Add a `list` method to the KVStore class. (#224)
    * Add a list method to KV Store
    
    * Respond to comments

--- tests/steamship_tests/utils/test_kv_store.py
+++ tests/steamship_tests/utils/test_kv_store.py
@@ -45,11 +45,29 @@ def test_key_value_store(client: Steamship):
     assert kv.get(key2) == value_2
     assert kv.get(key2) != value
 
+    # List all the keys
+    res = kv.items()
+    assert len(res) == 2
+    assert (key1, value) in res
+    assert (key2, value_2) in res
+
+    # List just the keys with val1
+    res = kv.items(filter_keys=[key1])
+    assert len(res) == 1
+    assert (key1, value) in res
+    assert (key2, value_2) not in res
+
     # Overwite the new one
     kv.set(key2, value_3)
     got = kv.get(key2)
     got_2 = kv.get(key2)
     got_3 = kv.get(key2)
+
+    # List just the keys with val1
+    res = kv.items(filter_keys=[key2])
+    assert len(res) == 1
+    assert (key1, value) not in res
+    assert (key2, value_3) in res
 
     assert got == value_3
     assert got != value_2
