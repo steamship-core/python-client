99e4f1dbdf04b6138fbef65ab0ec25554ef1d568
Dave Kolas <4643013+dkolas@users.noreply.github.com>
2022-12-21 15:04:58 -0600

    [SHIP-511] Add easier version of PluginInstance.train (#243)

--- tests/steamship_tests/plugin/integration/test_e2e_trainable_tagger.py
+++ tests/steamship_tests/plugin/integration/test_e2e_trainable_tagger.py
@@ -110,3 +110,81 @@ def test_e2e_trainable_tagger_lambda_training(client: Steamship):
                 assert res.output.file.tags is not None
                 assert len(res.output.file.tags) == len(KEYWORDS)
                 assert sorted([tag.kind for tag in res.output.file.tags]) == sorted(KEYWORDS)
+
+
+@pytest.mark.usefixtures("client")
+def test_e2e_trainable_tagger_lambda_training_new_train_params(client: Steamship):
+    version_config_template = {
+        "text_column": {"type": "string"},
+        "tag_columns": {"type": "string"},
+        "tag_kind": {"type": "string"},
+    }
+    instance_config = {"text_column": "Message", "tag_columns": "Category", "tag_kind": "Intent"}
+
+    csv_blockifier_path = PLUGINS_PATH / "blockifiers" / "csv_blockifier.py"
+    trainable_tagger_path = PLUGINS_PATH / "taggers" / "plugin_trainable_tagger.py"
+
+    # Make a blockifier which will generate our trainable corpus
+    with deploy_plugin(
+        client,
+        csv_blockifier_path,
+        "blockifier",
+        version_config_template=version_config_template,
+        instance_config=instance_config,
+    ) as (plugin, version, instance):
+        with upload_file(client, "utterances.csv") as file:
+            assert len(file.refresh().blocks) == 0
+            # Use the plugin we just registered
+            file.blockify(plugin_instance=instance.handle).wait()
+            assert len(file.refresh().blocks) == 5
+
+            # Now make a trainable tagger to train on those tags
+            with deploy_plugin(
+                client, trainable_tagger_path, "tagger", training_platform=HostingType.LAMBDA
+            ) as (tagger, tagger_version, tagger_instance):
+
+                train_result = tagger_instance.train(
+                    export_query='kind "foo1"',
+                    training_params={
+                        "keyword_list": KEYWORDS  # This is a key defined by the test model we're training
+                    },
+                )
+                train_result.wait()
+
+                # At this point, the PluginInstance will have written a parameter file to disk. We should be able to
+                # retrieve it since we know that it is tagged as the `default`.
+
+                checkpoint = ModelCheckpoint(
+                    client=client,
+                    handle="default",
+                    plugin_instance_id=tagger_instance.id,
+                )
+                checkpoint_path = checkpoint.download_model_bundle()
+                assert checkpoint_path.exists()
+                keyword_path = Path(checkpoint_path) / TestTrainableTaggerModel.KEYWORD_LIST_FILE
+                assert keyword_path.exists()
+                with open(keyword_path, "r") as f:
+                    params = json.loads(f.read())
+                    assert params == KEYWORDS
+
+                logging.info("Waiting 15 seconds for instance to deploy.")
+                import time
+
+                time.sleep(15)
+
+                # If we're here, we have verified that the plugin instance has correctly recorded its parameters
+                # into the pluginData bucket under a path unique to the PluginInstnace/ModelCheckpoint.
+
+                # Now we'll attempt to USE this plugin. This plugin's behavior is to simply tag any file with the
+                # tags that parameter it. Since those tags are (see above) ["product", "coupon"] we should expect
+                # this tagger to apply those tags to any file provided to it.
+
+                # First we'll create a file
+                test_doc = "Hi there"
+                res = tagger_instance.tag(doc=test_doc)
+                res.wait()
+                assert res.output is not None
+                assert res.output.file is not None
+                assert res.output.file.tags is not None
+                assert len(res.output.file.tags) == len(KEYWORDS)
+                assert sorted([tag.kind for tag in res.output.file.tags]) == sorted(KEYWORDS)
