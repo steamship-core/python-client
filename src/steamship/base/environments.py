cc7b6483e670db850ba040bcbba4cb4fcc9385d3
Ted Benson <ted@steamship.com>
2023-01-25 15:16:36 -0500

    WIP

--- src/steamship/base/environments.py
+++ src/steamship/base/environments.py
@@ -1,19 +1,61 @@
 import os
 from enum import Enum
 
+from steamship import Configuration, SteamshipError
+
 
 class RuntimeEnvironments(str, Enum):
     REPLIT = "replit"
     LOCALHOST = "localhost"
 
 
-def check_environment(env: RuntimeEnvironments, exit_unless_key: bool = True):
-    api_key = os.environ.get("STEAMSHIP_API_KEY")
+def _interactively_get_key(env: RuntimeEnvironments):
+    if env == RuntimeEnvironments.REPLIT:
+        print(
+            """To run this Replit, you will need a Steamship API Key.
 
-    if not api_key:
-        if env == RuntimeEnvironments.REPLIT:
-            print(
-                """To run this Replit, you will need a Steamship API Key.
+Visit http://steamship.com/account/api to fetch one, and then provide below:
+"""
+        )
+    elif env == RuntimeEnvironments.LOCALHOST:
+        print(
+            """To run this script, you will need a Steamship API Key.
+
+        Visit http://steamship.com/account/api to fetch one, and then provide below:
+"""
+        )
+
+    api_key = input("API Key: ")
+
+    while len(api_key.strip()) == 0:
+        api_key = input("API Key: ")
+
+    os.environ["STEAMSHIP_API_KEY"] = api_key
+
+    if env == RuntimeEnvironments.REPLIT:
+        print(
+            """
+This key is set temporarily. In the future, you can:
+- Set the STEAMSHIP_API_KEY Replit Secret
+- Close and re-open any Replit shells to make sure secrets are refreshed.
+
+"""
+        )
+    elif env == RuntimeEnvironments.LOCALHOST:
+        print(
+            """
+This key is set temporarily. In the future, you can:
+- Set the STEAMSHIP_API_KEY environment variable
+- Run `ship login` to create a ~/.steamship.json credential file
+
+"""
+        )
+
+
+def _report_error_and_exit(env: RuntimeEnvironments):
+    if env == RuntimeEnvironments.REPLIT:
+        print(
+            """To run this Replit, you will need a Steamship API Key.
 
 1) If you're viewing someone else's Replit, clone it
 
@@ -24,10 +66,10 @@ def check_environment(env: RuntimeEnvironments, exit_unless_key: bool = True):
 4) Close and re-open any shells to make sure your new secret is available
 
 Then try running again!"""
-            )
-        elif env == RuntimeEnvironments.LOCALHOST:
-            print(
-                """To run this script, you will need a Steamship API Key.
+        )
+    elif env == RuntimeEnvironments.LOCALHOST:
+        print(
+            """To run this script, you will need a Steamship API Key.
 
 1) Visit https://steamship.com/account/api to get a key
 
@@ -37,6 +79,28 @@ Then try running again!
 
 If you have pip-installed `steamship`, you can also try setting your key by simply running `ship login`.
 """
-            )
-        if exit_unless_key:
-            exit(-1)
+        )
+    exit(-1)
+
+
+def check_environment(env: RuntimeEnvironments, interactively_set_key: bool = True):
+    # This will try loading from STEAMSHIP_API_KEY and also ~/.steamship.json
+    try:
+        config = Configuration()
+
+        # If an API key is set, we're good to go!
+        if config.api_key:
+            return
+    except SteamshipError:
+        # The Configuration object will throw an error if there is no API Key found.
+        # Since that error is expected from the context of this function, we pass on it to handle it in a more
+        # user-interactive way.
+        pass
+
+    # If we're hot-loading config, do it here!
+    if interactively_set_key:
+        _interactively_get_key(env)
+        return
+
+    # If we're still here, we're not interactively setting the key. Display an error message and exit.
+    _report_error_and_exit(env)
