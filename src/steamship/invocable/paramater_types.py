2cdbfd3aabe375ef9933e1e8e422f195fac8384d
Ted Benson <ted@steamship.com>
2023-03-17 09:41:49 -0400

    Fileurl type

--- src/steamship/invocable/paramater_types.py
+++ src/steamship/invocable/paramater_types.py
@@ -2,3 +2,9 @@ class longstr(str):  # noqa: N801
     """Long string functions mostly as a type annotation for the web."""
 
     pass
+
+
+class fileurl(str):  # noqa: N801
+    """Type function that, if used in a package method argument, will cause a file upload widget to appear."""
+
+    pass
