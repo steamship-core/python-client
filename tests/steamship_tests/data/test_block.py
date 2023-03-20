05278cd17a2b8fda91f84178caaa692ed5c0d306
Dave Kolas <4643013+dkolas@users.noreply.github.com>
2023-03-20 08:15:22 -0500

    dir returns sdk version (#319)
    This PR allows calls to `__dir__` to include the SDK version being used.
    This is a prereq for having engine behavior depend on the SDK version,
    ex. when we introduce instance initialization.

--- tests/steamship_tests/data/test_block.py
+++ tests/steamship_tests/data/test_block.py
@@ -135,3 +135,20 @@ def test_create_with_tags(client: Steamship):
         tags=[Tag(kind="bar", name="foo"), Tag(kind="baz", name="foo")],
     )
     assert len(new_block.tags) == 2
+
+
+@pytest.mark.usefixtures("client")
+def test_append_is_present(client: Steamship):
+    file = File.create(client, blocks=[Block(text="first")])
+    assert len(file.blocks) == 1
+
+    file.append_block(text="second")
+    assert len(file.blocks) == 2
+
+    file.refresh()
+    assert len(file.blocks) == 2
+
+    my_file = File.create(client, handle="my_file", content="")
+    assert len(my_file.blocks) == 0
+    my_file.append_block(text="first")
+    assert len(my_file.blocks) == 1
