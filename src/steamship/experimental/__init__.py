a294d5b89b54d3cf4abf1ed4791d9a79b34d9879
Ted Benson <ted@steamship.com>
2023-04-18 00:51:17 -0400

    Easy Blockify

--- src/steamship/experimental/__init__.py
+++ src/steamship/experimental/__init__.py
@@ -7,8 +7,6 @@ In general, try to avoid introducing new dependencies to requirements.txt, but o
 to try out new metaphors and helper classes!
 """
 
-from .easy import scrape
+from .easy import blockify, scrape
 
-__all__ = [
-    "scrape",
-]
+__all__ = ["scrape", "blockify"]
--- src/steamship/experimental/easy/__init__.py
+++ src/steamship/experimental/easy/__init__.py
@@ -1,5 +1,4 @@
+from .blockify import blockify
 from .scrape import scrape
 
-__all__ = [
-    "scrape",
-]
+__all__ = ["scrape", "blockify"]
