600164cf9329441730c79222655cf4b7c1049582
Douglas Reid <douglas-reid@users.noreply.github.com>
2023-05-01 12:00:47 -0700

    feat: add 'ship use' command for easy instance creation (#340)
    This adds a basic command to the CLI to allow for rapid instance
    creation. It must be run from a directory containing a Steamship
    manifest file.
    
    Configuration may be passed as a JSON string, or as a path to JSON file.
    
    NOTE: At the moment, this must be run after 'ship deploy', as no attempt
    is made to create a package/plugin before creation of an instance. This
    could be addressed, if desired.
    
    ---------
    
    Co-authored-by: Douglas Reid <doug@steamship.com>

--- src/steamship/utils/utils.py
+++ src/steamship/utils/utils.py
@@ -1,5 +1,7 @@
 from typing import Any, Dict, Optional
 
+from steamship.utils.metadata import hash_dict
+
 
 def safe_get(d: Dict, key: str, default: Any = None) -> Optional[Any]:
     """Safely a value from dictionairy using a specific key"""
@@ -10,3 +12,10 @@ def format_uri(uri: Optional[str]) -> Optional[str]:
     if uri is not None and not uri.endswith("/"):
         uri += "/"
     return uri
+
+
+def create_instance_handle(
+    invocable_handle: str, version_handle: str, invocable_config: Optional[Dict[str, Any]]
+) -> str:
+    """Create an instance handle deterministically from package/plugin metadata and configuration."""
+    return f"{invocable_handle}-{hash_dict({**invocable_config, 'version': version_handle})}"
