3d3c22de7e4e455b401114625de9a9966d55f4b2
dkolas <4643013+dkolas@users.noreply.github.com>
2023-05-10 10:19:05 -0400

    Align local handler and lambda_handler

--- src/steamship/cli/local_server/handler.py
+++ src/steamship/cli/local_server/handler.py
@@ -1,190 +1,32 @@
 import json
 import logging
 import re
-import sys
-import uuid
-from http import HTTPStatus, server
-from typing import Callable, Dict, Optional, Type
+from http import server
+from typing import Dict, Optional, Type
 from urllib.parse import parse_qs, urlparse
 
-from steamship import Configuration, MimeTypes, Steamship, SteamshipError
+from steamship import MimeTypes, Steamship
 from steamship.data.user import User
-from steamship.data.workspace import SignedUrl
 from steamship.invocable import (
     Invocable,
     InvocableRequest,
-    InvocableResponse,
     Invocation,
     InvocationContext,
     LoggingConfig,
     PackageService,
 )
-from steamship.utils.signed_urls import upload_to_signed_url
-
-
-def internal_handler(  # noqa: C901
-    invocable_cls_func: Callable[[], Type[Invocable]],
-    event: Dict,
-    client: Steamship,
-    invocation_context: InvocationContext,
-) -> InvocableResponse:
-    invocable_class: Type[Invocable] = invocable_cls_func()
-
-    try:
-        request = InvocableRequest.parse_obj(event)
-    except SteamshipError as se:
-        logging.exception(se)
-        return InvocableResponse.from_obj(se)
-    except Exception as ex:
-        logging.exception(ex)
-        return InvocableResponse.error(
-            code=HTTPStatus.INTERNAL_SERVER_ERROR,
-            message="Plugin/App handler was unable to parse inbound request.",
-            exception=ex,
-        )
-
-    if request and request.invocation:
-        error_prefix = (
-            f"[ERROR - {request.invocation.http_verb} {request.invocation.invocation_path}] "
-        )
-    else:
-        error_prefix = "[ERROR - ?VERB ?PATH] "
-
-    if request.invocation.invocation_path == "/__dir__":
-        # Return the DIR result without (1) Constructing invocable_cls or (2) Parsing its config (in the constructor)
-        try:
-            return InvocableResponse(json=invocable_class.__steamship_dir__(invocable_class))
-        except SteamshipError as se:
-            logging.exception(se)
-            return InvocableResponse.from_obj(se)
-        except Exception as ex:
-            logging.exception(ex)
-            return InvocableResponse.error(
-                code=HTTPStatus.INTERNAL_SERVER_ERROR,
-                prefix=error_prefix,
-                message="Unable to initialize package/plugin.",
-                exception=ex,
-            )
-
-    try:
-        invocable = invocable_cls_func()(
-            client=client, config=request.invocation.config, context=invocation_context
-        )
-        # TODO: We don't want to run this every time, but for now we are.
-        logging.info("Running __instance_init__")
-        invocable.instance_init()
-    except SteamshipError as se:
-        logging.exception(se)
-        return InvocableResponse.from_obj(se)
-    except Exception as ex:
-        logging.exception(ex)
-        return InvocableResponse.error(
-            code=HTTPStatus.INTERNAL_SERVER_ERROR,
-            prefix=error_prefix,
-            message="Unable to initialize package/plugin.",
-            exception=ex,
-        )
-
-    if not invocable:
-        return InvocableResponse.error(
-            code=HTTPStatus.INTERNAL_SERVER_ERROR,
-            prefix=error_prefix,
-            message="Unable to construct package/plugin for invocation.",
-        )
-
-    try:
-        response = invocable(request)
-        return InvocableResponse.from_obj(response)
-    except SteamshipError as se:
-        logging.exception(se)
-        se.message = f"{error_prefix}{se.message}"
-        return InvocableResponse.from_obj(se)
-    except Exception as ex:
-        logging.exception(ex)
-        return InvocableResponse.error(
-            code=HTTPStatus.INTERNAL_SERVER_ERROR,
-            prefix=error_prefix,
-            exception=ex,
-        )
-
-
-def handler(bound_internal_handler, event: Dict, _: Dict = None) -> dict:  # noqa: C901
-    invocation_context_dict = event.get("invocationContext")
-    if invocation_context_dict is None:
-        return InvocableResponse.error(
-            code=HTTPStatus.INTERNAL_SERVER_ERROR,
-            message="Plugin/App handler did not receive an invocation context.",
-        ).dict(by_alias=True)
-
-    invocation_context = InvocationContext.parse_obj(invocation_context_dict)
-
-    # At the point in the code, the root log level seems to default to WARNING unless set to INFO, even with
-    # the BasicConfig setting to INFO above.
-    logging.root.setLevel(logging.INFO)
-
-    # These log statements intentionally go to the logging handler pre-remote attachment, to debug logging configuration issues
-    logging.info(f"Invocation context: {invocation_context}")
-
-    try:
-        # Config will accept `workspace_id` as passed from the Steamship Engine, whereas the `Steamship`
-        # class itself is limited to accepting `workspace` (`config.workspace_handle`) since that is the manner
-        # of interaction ideal for developers.
-        config = Configuration(**event.get("clientConfig", {}))
-        client = Steamship(config=config, trust_workspace_config=True)
-    except SteamshipError as se:
-        logging.exception(se)
-        return InvocableResponse.from_obj(se).dict(by_alias=True)
-    except Exception as ex:
-        logging.exception(ex)
-        return InvocableResponse.error(
-            code=HTTPStatus.INTERNAL_SERVER_ERROR,
-            message="Plugin/App handler was unable to create Steamship client.",
-            exception=ex,
-        ).dict(by_alias=True)
-
-    response = bound_internal_handler(event, client, invocation_context)
-
-    result = response.dict(by_alias=True, exclude={"client"})
-
-    # When created with data > 4MB, data is uploaded to a bucket.
-    # This is a very ugly way to get the deep size of this object
-    data = json.dumps(result.get("data", None)).encode("UTF-8")
-    data_size = sys.getsizeof(data)
-    logging.info(f"Response data size {data_size}")
-    if data_size > 4e6 and invocation_context.invocable_type == "plugin":
-        logging.info("Response data size >4MB, must upload to bucket")
-        filepath = str(uuid.uuid4())
-        signed_url = (
-            client.get_workspace()
-            .create_signed_url(
-                SignedUrl.Request(
-                    bucket=SignedUrl.Bucket.PLUGIN_DATA,
-                    filepath=filepath,
-                    operation=SignedUrl.Operation.WRITE,
-                )
-            )
-            .signed_url
-        )
-
-        logging.info(f"Got signed url for writing: {signed_url}")
-
-        upload_to_signed_url(signed_url, data)
-
-        # Now remove raw data and replace with bucket
-        del result["data"]
-        result["dataBucket"] = SignedUrl.Bucket.PLUGIN_DATA.value
-        result["dataFilepath"] = filepath
-
-    return result
+from steamship.invocable.lambda_handler import handler, internal_handler
 
 
 def create_safe_handler(invocable: Type[Invocable] = None):
     """Mimics the create_safe_handler function in lambda_handler for parallelism."""
     invocable_getter = lambda: invocable  # noqa: E731
     bound_internal_handler = lambda event, client, context: internal_handler(  # noqa: E731
-        invocable_getter, event, client, context
+        invocable_getter, event, client, context, call_instance_init=True
     )
-    return lambda event, context=None: handler(bound_internal_handler, event, context)
+    return lambda event, context=None: handler(
+        bound_internal_handler, event, context, running_locally=True
+    )
 
 
 def make_handler(  # noqa: C901
