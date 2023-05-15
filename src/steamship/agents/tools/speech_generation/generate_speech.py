3e5e4289a60f842a0c569b17b6640f53475b65e4
Ted Benson <ted@steamship.com>
2023-05-15 16:19:05 -0400

    Generate Speech Tool

--- src/steamship/agents/tools/speech_generation/generate_speech.py
+++ src/steamship/agents/tools/speech_generation/generate_speech.py
@@ -1,54 +1,81 @@
 """Tool for generating images."""
-import json
-import logging
-from typing import Optional
+from typing import List
 
-from steamship import Steamship
-from steamship.base.error import SteamshipError
-from steamship.experimental.tools.tool import Tool
+from steamship import Block
+from steamship.agents.agent_context import AgentContext, DebugAgentContext
+from steamship.agents.agents import Tool
+from steamship.agents.debugging import tool_repl
 
 
 class GenerateSpeechTool(Tool):
-    """Tool used to generate images from a text-prompt."""
+    """Tool to generate audio from text."""
 
-    name: str = "GenerateSpokenAudio"
-    description: str = (
-        "Used to generate spoken audio from text prompts. Only use if the user has asked directly for a "
-        "an audio version of output. When using this tool, the input should be a plain text string containing the "
-        "content to be spoken."
-    )
+    def __init__(self, **kwargs):
+        """Generates audio from text."""
+        kwargs["name"] = kwargs.get("name", "GenerateSpokenAudio")
+        kwargs["human_description"] = kwargs.get(
+            "human_description", "Generates spoken audio from text."
+        )
+        kwargs["ai_description"] = kwargs.get(
+            "ai_description",
+            (
+                "Used to generate spoken audio from text prompts. Only use if the user has asked directly for a "
+                "an audio version of output. When using this tool, the input should be a plain text string containing the "
+                "content to be spoken."
+            ),
+        )
+        super().__init__(**kwargs)
 
-    client: Steamship
+    def run(self, tool_input: List[Block], context: AgentContext) -> List[Block]:
+        """Generates audio for each text block provided.
 
-    def __init__(self, client: Steamship, plugin_handle: Optional[str] = "elevenlabs"):
-        self.plugin_handle = plugin_handle
-        self.client = client
-        self.elevenlabs = client.use_plugin(plugin_handle=self.plugin_handle, config={})
+        Waits, synchronously, for completion.
+        TODO: We should probably make this async.
 
-    def should_preempt_agent(self, prompt: str) -> float:
-        """Preempt when an inbound message begins with 'speak '."""
-        if prompt.strip().startswith("speak "):
-            return 1.0
-        return 0.0
+        Inputs
+        ------
+        input: List[Block]
+            A list of blocks to be rewritten if text-containing.
+        context: AgentContext
+            The active AgentContext.
 
-    def preempt_agent_prompt(self, prompt: str) -> str:
-        """Stripe 'speak ' from the inbound message."""
-        prefix = "speak "
-        if prompt.startswith(prefix):
-            prompt = prompt[len(prefix) :]
+        Output
+        ------
+        output: List[Blocks]
+            A list of blocks containing audio content.
+        """
 
-        return prompt.strip()
+        # TODO: Make this an option in the tool
+        plugin_handle = "elevenlabs"
 
-    def run(self, prompt: str) -> str:
-        """Respond to LLM prompt."""
-        logging.info(f"[{self.name}] {prompt}")
-        if not isinstance(prompt, str):
-            prompt = json.dumps(prompt)
-        task = self.elevenlabs.generate(text=prompt, append_output_to_file=True)
-        task.wait()
-        blocks = task.output.blocks
-        logging.info(f"[{self.name}] got back {len(blocks)} blocks")
-        if len(blocks) > 0:
-            logging.info(f"[{self.name}] audio size: {len(blocks[0].raw())}")
-            return blocks[0].id
-        raise SteamshipError(f"[{self.name}] Tool unable to generate audio!")
+        # Assumed at this point to be an audio generator.
+        generator = context.client.use_plugin(plugin_handle=plugin_handle, config={})
+
+        output = []
+        for block in tool_input:
+            if not block.is_text():
+                continue
+
+            prompt = block.text
+            task = generator.generate(text=prompt, append_output_to_file=True)
+            task.wait()
+            blocks = task.output.blocks
+
+            context.append_log(f"[{self.name}] got back {len(blocks)} blocks")
+            if len(blocks) > 0:
+                context.append_log(f"[{self.name}] audio size: {len(blocks[0].raw())}")
+                # TODO: This is how we were doing it.. but it feels like with this new interface
+                # perhaps we should return the actual block?
+                output.append(Block(text=f"{blocks[0].id}"))
+
+        return output
+
+
+def main():
+    with DebugAgentContext.temporary() as context:
+        tool = GenerateSpeechTool()
+        tool_repl(tool, context)
+
+
+if __name__ == "__main__":
+    main()
