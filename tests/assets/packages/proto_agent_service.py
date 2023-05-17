d6742943adae0c81d411bdaac01eded2989be8b7
Ted Benson <ted@steamship.com>
2023-05-17 17:59:21 -0400

    WIP

--- tests/assets/packages/proto_agent_service.py
+++ tests/assets/packages/proto_agent_service.py
@@ -1,214 +1,193 @@
-# import base64
-# import io
-# from typing import Any, Dict, List, Optional
-#
-# from pydantic import BaseModel
-#
-# from steamship import SteamshipError, Task, Block, File
-# from steamship.base.mime_types import MimeTypes
-# from steamship.base.model import CamelModel
-# from steamship.client import Steamship
-# from steamship.data.user import User
-# from steamship.invocable import (
-#     InvocableResponse,
-#     InvocationContext,
-#     PackageService,
-#     create_handler,
-#     get,
-#     post,
-# )
-#
-# class ToolRegistry(CamelModel):
-#     tools: Dict[str, Tool]
-#
-#     def add_tool(self, tool: Tool):
-#         tools[tool.name]
-#
-#
-# class ToolBinding(CamelModel):
-#     """The combination of a Tool and its Input"""
-#     tool_name: str # Tool Name
-#
-#     # The tool input may be an inlined list of blocks.
-#     tool_input_inline: Optional[List[Block]]
-#
-#     # The tool input may be the output of a known file.
-#     tool_input_file: Optional[str] # UUID
-#
-#     # The tool input may be the output of a known task.
-#     tool_input_task: Optional[str] # UUID
-#
-#     # If tool input is a task, optional tool to peform post-processing
-#     tool_input_task_postprocesor: Optional[str] # Tool Name
-#
-#     def get_tool_input(self, client: Steamship) -> List[Block]:
-#         if self.inline_tool_input:
-#             return self.inline_tool_input
-#         elif self.file_tool_input:
-#             file = File.get(client, _id=self.file_tool_input)
-#             return file.blocks
-#         elif self.tool_input_task:
-#             task = Task.get(client, _id=self.tool_input_task)
-#
-#             # TODO: In this case, post-processing is required.
-#             return task.output
-#
-#
-#
-# class Action(CamelModel):
-#     name: str
-#     tool_binding: ToolBinding
-#     outputs: Optional[ToolOutputs] = []
-#
-#     def __init__(self, tool_binding: ToolBinding, context: AgentContext):
-#         self.tool_binding = tool_binding
-#         self.context = context
-#
-#     def is_async(self) -> bool:
-#         return isinstance(self.tool_binding.tool, AsyncTool)
-#
-#     def is_finish(self) -> bool:
-#         return False
-#
-#
-#
-# class TestPackage(PackageService):
-#     def __init__(
-#         self,
-#         client: Steamship = None,
-#         config: Dict[str, Any] = None,
-#         context: InvocationContext = None,
-#     ):
-#         super().__init__(client, config, context)
-#         self.index = None
-#
-#     @get("resp_string")
-#     def resp_string(self) -> InvocableResponse[str]:
-#         return InvocableResponse(string="A String")
-#
-#     @get("resp_dict")
-#     def resp_dict(self) -> InvocableResponse[dict]:
-#         return InvocableResponse(json={"string": "A String", "int": 10})
-#
-#     @get("resp_obj")
-#     def resp_obj(self) -> InvocableResponse[dict]:
-#         return InvocableResponse(json=TestObj(name="Foo"))
-#
-#     @get("resp_binary")
-#     def resp_binary(self) -> InvocableResponse[bytes]:
-#         _bytes = base64.b64decode(PALM_TREE_BASE_64)
-#         return InvocableResponse(_bytes=_bytes)
-#
-#     @get("resp_bytes_io")
-#     def resp_bytes_io(self) -> InvocableResponse[bytes]:
-#         _bytes = base64.b64decode(PALM_TREE_BASE_64)
-#         return InvocableResponse(_bytes=io.BytesIO(_bytes))
-#
-#     @get("resp_image")
-#     def resp_image(self) -> InvocableResponse[bytes]:
-#         _bytes = base64.b64decode(PALM_TREE_BASE_64)
-#         return InvocableResponse(_bytes=_bytes, mime_type=MimeTypes.PNG)
-#
-#     @get(
-#         "greet", public=True, timeout=10, identifier="foo", body=98.6, not_there={"not": "included"}
-#     )
-#     def greet1(self, name: str = "Person") -> InvocableResponse[str]:
-#         return InvocableResponse(string=f"Hello, {name}!")
-#
-#     @post("greet")
-#     def greet2(self, name: str = "Person") -> InvocableResponse[str]:
-#         return InvocableResponse(string=f"Hello, {name}!")
-#
-#     @post("public_post_greet", public=True)
-#     def public_post_greet(self, name: str = "Person") -> InvocableResponse[str]:
-#         return InvocableResponse(string=f"Hello, {name}!")
-#
-#     @get("public_get_greet", public=True)
-#     def public_get_greet(self, name: str = "Person") -> InvocableResponse[str]:
-#         return InvocableResponse(string=f"Hello, {name}!")
-#
-#     @post("future_greet")
-#     def future_greet(self, name: str = "Person") -> InvocableResponse[Task]:
-#         task_1 = self.invoke_later("greet", arguments={"name": name})
-#         return InvocableResponse(json=task_1)
-#
-#     @post("future_greet_then_greet_again")
-#     def future_greet_then_greet_again(self, name: str = "Person") -> InvocableResponse[Task]:
-#         task_1 = self.invoke_later("greet", arguments={"name": name})
-#         task_2 = self.invoke_later("greet", arguments={"name": f"{name} 2"}, wait_on_tasks=[task_1])
-#         return InvocableResponse(json=task_2)
-#
-#     @get("workspace")
-#     def workspace(self) -> InvocableResponse[str]:
-#         return InvocableResponse(string=self.client.config.workspace_id)
-#
-#     @post("raise_steamship_error")
-#     def raise_steamship_error(self) -> InvocableResponse[str]:
-#         raise SteamshipError(message="raise_steamship_error")
-#
-#     @post("raise_python_error")
-#     def raise_python_error(self) -> InvocableResponse[str]:
-#         raise Exception("raise_python_error")
-#
-#     @post("user_info")
-#     def user_info(self) -> InvocableResponse[dict]:
-#         user = User.current(self.client)
-#         return InvocableResponse(json={"handle": user.handle})
-#
-#     @post("json_with_status")
-#     def json_with_status(self) -> InvocableResponse[dict]:
-#         """Our base client tries to be smart with parsing things that look like SteamshipResponse objects, but there's
-#         a problem with this when our packages response with a JSON string of the sort {"status": "foo"} -- the Client
-#         will unhelpfully try to coerce that string value into a Task object and fail. This method helps us test that
-#         we are handling it properly."""
-#         return InvocableResponse(json={"status": "a string"})
-#
-#     @get("config")
-#     def get_config(self) -> InvocableResponse[dict]:
-#         """This is called get_config because there's already `.config` object on the class."""
-#         return InvocableResponse(
-#             json={
-#                 "workspaceId": self.client.config.workspace_id,
-#                 "appBase": self.client.config.app_base,
-#                 "apiBase": self.client.config.api_base,
-#                 "apiKey": self.client.config.api_key.get_secret_value(),
-#             }
-#         )
-#
-#     @post("learn")
-#     def learn(self, fact: str = None) -> InvocableResponse[dict]:
-#         """Learns a new fact."""
-#         if fact is None:
-#             return InvocableResponse.error(500, "Empty fact provided to learn.")
-#
-#         if self.index is None:
-#             return InvocableResponse.error(500, "Unable to initialize QA index.")
-#
-#         res = self.index.embed(fact)
-#
-#         if res.error:
-#             # Steamship error messages can be passed straight
-#             # back to the user
-#             return InvocableResponse(error=res.error)
-#         return InvocableResponse(json=res.data)
-#
-#     @post("query")
-#     def query(self, query: str = None, k: int = 1) -> InvocableResponse[dict]:
-#         """Learns a new fact."""
-#         if query is None:
-#             return InvocableResponse.error(500, "Empty query provided.")
-#
-#         if self.index is None:
-#             return InvocableResponse.error(500, "Unable to initialize QA index.")
-#
-#         res = self.index.query(query=query, k=k)
-#
-#         if res.error:
-#             # Steamship error messages can be passed straight
-#             # back to the user
-#             return InvocableResponse(error=res.error)
-#
-#         return InvocableResponse(json=res.data)
-#
-#
-# handler = create_handler(TestPackage)
+"""Intended to be a small, self-contained test of whether we can plumb various tool + task decompositions."""
+import json
+from typing import Any, Dict, List, Optional, Union, cast
+
+from steamship import Block, File, SteamshipError, Task, TaskState
+from steamship.agents.context import AgentContext
+from steamship.agents.tool import Tool
+from steamship.base.model import CamelModel
+from steamship.client import Steamship
+from steamship.invocable import (
+    InvocableResponse,
+    InvocationContext,
+    PackageService,
+    create_handler,
+    post,
+)
+
+
+class ToolRegistry(CamelModel):
+    available_tools: Dict[str, Tool]
+
+    def get_tool(self, name: str) -> Tool:
+        if name not in self.available_tools:
+            raise SteamshipError(message=f"Tool {name} was not in registry")
+        return self.available_tools[name]
+
+    def add_tool(self, tool: Tool):
+        self.available_tools[tool.name] = tool
+
+
+def task_output_as_blocks(task: Task) -> List[Block]:
+    # Note: this would want to be a less brittle implementation, but the assumption here is that,
+    # without a post-processor, (1) the output MUST be a list of blocks, and (2) it will be stored in
+    # the Task as a string.
+    return [cast(Block, Block.parse_obj(block_dict)) for block_dict in json.loads(task.output)]
+
+
+class ToolBinding(CamelModel):
+    """The combination of a Tool and its Input"""
+
+    tool_name: str  # Tool Name
+
+    # The tool input may be an inlined list of blocks.
+    tool_input_inline: Optional[List[Block]]
+
+    # The tool input may be the output of a known file.
+    tool_input_file: Optional[str]  # UUID
+
+    # The tool input may be the output of a known task.
+    tool_input_task: Optional[str]  # UUID
+
+    # If tool input is a task, optional tool to peform post-processing
+    tool_input_task_postprocesor: Optional[str]  # Tool Name
+
+    # OUTPUT
+
+    # The tool output may be inlined
+    tool_output_inline: Optional[List[Block]]
+
+    # The tool output may be the result of a task
+    tool_output_task: Optional[str]
+
+    def get_tool_input(self, client: Steamship, tool_registry: ToolRegistry) -> List[Block]:
+        """Return the tool input, which may be inline, in a file, or the result of a task + optional post-processing."""
+        if self.tool_input_inline:
+            return self.tool_input_inline
+        elif self.tool_input_file:
+            file = File.get(client, _id=self.tool_input_file)
+            return file.blocks
+        elif self.tool_input_task:
+            task = Task.get(client, _id=self.tool_input_task)
+            if self.tool_input_task_postprocesor:
+                return tool_registry.get(self.tool_input_task_postprocesor).post_process(task)
+            else:
+                return task_output_as_blocks(task)
+
+    def get_tool_output(
+        self, client: Steamship, tool_registry: ToolRegistry, post_process: bool = False
+    ) -> List[Block]:
+        """Return the tool output.
+
+        There is an edge case: the FINAL step of a chain may require post-processing upon egress. But upon ingress
+        this can be handled on the get_tool_input side of things.
+
+        This edge case wouldn't exist if we just always had the tool post-process upon completion, but we need it to be
+        upon the start of the next task, or retrieval of the final value, since that's the moment when we resume control
+        from some externalized task (3rd party call, blockification, etc) that is unaware of the nature of this post-processing.
+        """
+        if self.tool_output_inline:
+            return self.tool_input_inline
+        elif self.tool_output_task:
+            task = Task.get(client, _id=self.tool_output_task)
+            if post_process:
+                return tool_registry.get(self.tool_name).post_process(task)
+            else:
+                return task_output_as_blocks(task)
+
+    def is_input_ready(self, client: Steamship) -> bool:
+        if self.tool_input_inline:
+            return True
+        elif self.tool_input_file:
+            return True  # Uncertain here.
+        elif self.tool_input_task:
+            task = Task.get(client, _id=self.tool_input_task)
+            return task.state == TaskState.succeeded
+
+    def is_output_ready(self, client: Steamship) -> bool:
+        if self.tool_output_inline:
+            return True
+        elif self.tool_output_task:
+            task = Task.get(client, _id=self.tool_output_task)
+            return task.state == TaskState.succeeded
+
+
+class Executor(CamelModel):
+    client: Steamship
+    tool_registry: ToolRegistry
+
+    def __init__(self, client: Steamship):
+        super().__init__()
+        self.client = client
+        self.tool_registry = ToolRegistry()
+
+    def run(
+        self, tool_binding: ToolBinding, context: AgentContext
+    ) -> Union[List[Block], Task[Any]]:
+        tool_input = tool_binding.get_tool_input(self.client, self.tool_registry)
+        tool = self.tool_registry.get(tool_binding.tool_name)
+        result = tool.run(tool_input, context)
+        return result
+
+
+class ToolThatAlwaysResponseSynchronouslyAndStatically(Tool):
+    name = "ToolThatAlwaysResponseSynchronouslyAndStatically"
+    ai_description = "ToolThatAlwaysResponseSynchronouslyAndStatically"
+    human_description = "ToolThatAlwaysResponseSynchronouslyAndStatically"
+
+    def run(self, tool_input: List[Block], context: AgentContext) -> Union[List[Block], Task[Any]]:
+        return [Block(text="This was Synchronous and Static")]
+
+
+class ToolThatAlwaysResponseSynchronouslyAndDynamically(Tool):
+    name = "ToolThatAlwaysResponseSynchronouslyAndStatically"
+    ai_description = "ToolThatAlwaysResponseSynchronouslyAndStatically"
+    human_description = "ToolThatAlwaysResponseSynchronouslyAndStatically"
+
+    def run(self, tool_input: List[Block], context: AgentContext) -> Union[List[Block], Task[Any]]:
+        ret = []
+        for block in tool_input:
+            ret.append(
+                Block(text=f"This was synchronous and Dynamic. Input block was: {block.text}")
+            )
+        return ret
+
+
+class AgentService(PackageService):
+    executor: Executor
+
+    def __init__(
+        self,
+        client: Steamship = None,
+        config: Dict[str, Any] = None,
+        context: InvocationContext = None,
+    ):
+        super().__init__(client, config, context)
+        self.executor = Executor(self.client)
+        self.executor.tool_registry.add_tool(ToolThatAlwaysResponseSynchronouslyAndStatically())
+        self.executor.tool_registry.add_tool(ToolThatAlwaysResponseSynchronouslyAndDynamically())
+
+    def _register_tool(self, tool: Tool):
+        self.tools[tool.name] = tool
+
+    @post("echo")
+    def echo(self, **kwargs) -> InvocableResponse[dict]:
+        return kwargs
+
+    @post("run_tool")
+    def run_tool(self, binding: ToolBinding) -> ToolBinding:
+        """Always return a Task that resolves to the eventual output of the workplan, even if the workplan was synchronous.
+
+        For ease of experimentation, I'm using ToolBinding as the envelope that wraps all requests and responses.
+        """
+        result = self.executor.run(binding)
+
+        if isinstance(result, Task):
+            binding.tool_output_task = result.task_id
+            return binding
+        else:
+            binding.tool_output_inline = result
+            return binding
+
+
+handler = create_handler(AgentService)
