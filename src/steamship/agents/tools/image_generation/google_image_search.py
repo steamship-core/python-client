fe8dc524ae60adadb6aa4a4384b420df2b4ad72d
Ted Benson <ted@steamship.com>
2023-05-22 15:17:35 -0400

    Google image search

---
+++ src/steamship/agents/tools/image_generation/google_image_search.py
@@ -0,0 +1,22 @@
+"""Tool for generating images."""
+from steamship.agents.tool import ImageGeneratorTool
+from steamship.utils.repl import ToolREPL
+
+
+class GoogleImageSearchTool(ImageGeneratorTool):
+    """Tool to generate images from text."""
+
+    name: str = "GoogleImageSearchTool"
+    human_description: str = "Fetches an image from Google Image Search."
+    ai_description = (
+        "Used to retrieve an image of a well known object, person, place, or idea. Only use if the user has asked directly for an image"
+        "image .",
+        "Input: a plain text string that describes an object, person, place, or idea. "
+        "Output: an image of that thing.",
+    )
+    generator_plugin_handle: str = "google-image-search"
+    generator_plugin_config: dict = {}
+
+
+if __name__ == "__main__":
+    ToolREPL(GoogleImageSearchTool()).run()
