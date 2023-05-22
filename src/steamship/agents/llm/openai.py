62ed92fb426d6571f802b850f4831841e2499376
Ted Benson <edward.benson@gmail.com>
2023-05-22 17:09:39 -0400

    Fix bug in model name (#376)

--- src/steamship/agents/llm/openai.py
+++ src/steamship/agents/llm/openai.py
@@ -3,14 +3,22 @@ from typing import List
 from steamship import Block, PluginInstance, Steamship
 from steamship.agents.base import LLM
 
+PLUGIN_HANDLE = "gpt4"
+
 
 class OpenAI(LLM):
     generator: PluginInstance
     client: Steamship
 
-    def __init__(self, client, model_name: str, *args, **kwargs):
+    def __init__(self, client, model_name: str = "gpt-3.5-turbo", *args, **kwargs):
+        """Create a new instance.
+
+        Valid model names are:
+         - gpt-4
+         - gpt-3.5-turbo
+        """
         client = client
-        generator = client.use_plugin(model_name)
+        generator = client.use_plugin(PLUGIN_HANDLE, config={"model": model_name})
         super().__init__(client=client, generator=generator, *args, **kwargs)
 
     def complete(self, prompt: str, stop: str) -> List[Block]:
