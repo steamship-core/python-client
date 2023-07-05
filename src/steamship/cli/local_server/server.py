a61755a64a12eb97b22289a65a09ea55d82097fe
Enias Cailliau <enias@steamship.com>
2023-07-05 11:35:23 +0200

    Fix local server

--- src/steamship/cli/local_server/server.py
+++ src/steamship/cli/local_server/server.py
@@ -2,8 +2,15 @@ import threading
 from socketserver import TCPServer
 from typing import Optional, Type
 
-from steamship.cli.local_server.handler import make_handler
-from steamship.invocable import Invocable
+from steamship import Steamship
+from steamship.cli.local_server.handler import create_safe_handler, make_handler
+from steamship.invocable import (
+    Invocable,
+    InvocableRequest,
+    Invocation,
+    InvocationContext,
+    LoggingConfig,
+)
 
 
 class SteamshipHTTPServer:
@@ -40,6 +47,7 @@ class SteamshipHTTPServer:
         invocable_instance_handle: str = None,
         config: dict = None,
         add_port_to_invocable_url: bool = True,  # The invocable url represents the external URL, which may be NGROK
+        workspace: str = None,
     ):
         self.invocable = invocable
         self.port = port
@@ -50,23 +58,39 @@ class SteamshipHTTPServer:
         self.invocable_instance_handle = invocable_instance_handle
         self.config = config
         self.add_port_to_invocable_url = add_port_to_invocable_url
+        self.workspace = workspace
 
     def start(self):
         """Start the server."""
-        self.server = TCPServer(
-            ("", self.port),
-            make_handler(
-                self.invocable,
-                self.port,
-                base_url=self.base_url,
-                default_api_key=self.default_api_key,
-                invocable_handle=self.invocable_handle,
-                invocable_version_handle=self.invocable_version_handle,
-                invocable_instance_handle=self.invocable_instance_handle,
-                config=self.config,
-                add_port_to_invocable_url=self.add_port_to_invocable_url,
-            ),
+
+        handler = make_handler(
+            self.invocable,
+            self.port,
+            base_url=self.base_url,
+            default_api_key=self.default_api_key,
+            invocable_handle=self.invocable_handle,
+            invocable_version_handle=self.invocable_version_handle,
+            invocable_instance_handle=self.invocable_instance_handle,
+            config=self.config,
+            add_port_to_invocable_url=self.add_port_to_invocable_url,
+            workspace=self.workspace,
         )
+        self.server = TCPServer(("", self.port), handler)
+        # We need to trigger the instance init.
+        context = InvocationContext(invocable_url=f"{self.base_url}/")
+
+        invocation = Invocation(
+            http_verb="POST", invocation_path="__dir__", arguments={}, config=self.config
+        )
+        event = InvocableRequest(
+            client_config=Steamship(api_key=self.default_api_key, workspace=self.workspace).config,
+            invocation=invocation,
+            logging_config=LoggingConfig(logging_host=None, logging_port=None),
+            invocation_context=context,
+        )
+        handler = create_safe_handler(self.invocable)
+        handler(event.dict(by_alias=True), context)
+
         self.server.serve_forever()
 
     def stop(self):
