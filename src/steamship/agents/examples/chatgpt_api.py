199146467c9629f8e59da025a3996977686814d4
Ted Benson <ted@steamship.com>
2023-07-15 00:02:49 +0800

    Add two agents for testing what's in the docs

--- src/steamship/agents/examples/chatgpt_api.py
+++ src/steamship/agents/examples/chatgpt_api.py
@@ -6,9 +6,17 @@ This will result in an agent that effectively acts like ChatGPT.
 from steamship.agents.functional import FunctionsBasedAgent
 from steamship.agents.llms.openai import ChatOpenAI
 from steamship.agents.service.agent_service import AgentService
+from steamship.utils.repl import AgentREPL
 
 
 class MyAssistant(AgentService):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self._agent = FunctionsBasedAgent(llm=ChatOpenAI(self.client), tools=[])
+
+
+if __name__ == "__main__":
+    AgentREPL(
+        MyAssistant,
+        agent_package_config={"botToken": "not-a-real-token-for-local-testing"},
+    ).run()
