4f2f08850633dd36a06622a3d1be29858aa3ce15
Max Freundlich <mfreundlich1@gmail.com>
2023-07-25 16:52:55 -0400

    feat: move scripts into bash file

--- docs/clear_markdown.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Delete all empty folders
-find "../../python-docs/pages" -type d -empty -delete
-
-# Delete all files that aren't named _meta.json
-find "../../python-docs/pages" -type f ! -name "_meta.json" -delete
