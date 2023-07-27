488b67b1fdc740996e68d285a98e9c692d5c97c7
dkolas <4643013+dkolas@users.noreply.github.com>
2023-07-27 10:07:27 -0400

    Allow tags to be returned in File Importers

--- tests/steamship_tests/plugin/integration/test_e2e_file_importer.py
+++ tests/steamship_tests/plugin/integration/test_e2e_file_importer.py
@@ -16,11 +16,20 @@ def test_e2e_importer(client: Steamship):
         instance,
     ):
         # The test FileImporter should always return a string file with contents TEST_DOC
-        file = File.create_with_plugin(client=client, plugin_instance=instance.handle)
-        file.wait()
-        file = file.output
+        file_task = File.create_with_plugin(client=client, plugin_instance=instance.handle)
+        file = file_task.wait()
+
+        assert len(file.tags) == 1
+        assert file.tags[0].kind == "test-kind"
+        assert file.tags[0].name == "test-name"
+
         # Now fetch the data from Steamship and assert that it is the SAME as the data the FileImporter creates
         data = file.raw()
         assert data.decode("utf-8") == TEST_DOC
+
+        fetched_file = File.get(client, _id=file.id)
+        assert len(fetched_file.tags) == 1
+        assert fetched_file.tags[0].kind == "test-kind"
+        assert fetched_file.tags[0].name == "test-name"
 
         file.delete()
