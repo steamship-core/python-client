cbf624fc2c1c21e8a96fa9cc29fc2a5d3d3f6213
Ted Benson <edward.benson@gmail.com>
2023-08-30 12:03:38 -0400

    Tag ChatHistory blocks and files  (#531)
    We don't currently Tag ChatHistory files as being ChatHistory files.
    That means that the Web UI can't show a list of prior chats and let the
    user resume or browse one.
    
    This PR adds a tag constant for ChatHistory and tags new ChatHistory
    files with it when they are created.
    
    ### Along for the ride on this PR
    
    - Remove unused code from `experimental`
    - Consolidate duplicated `get_tag_value_key` methods into a single
    `tag_util.py` file.
    - Extract `build_chat_history_for_tool` function from `next_action` to
    enable testing on its own and possible resuse

--- tests/steamship_tests/agents/test_chat_history.py
+++ tests/steamship_tests/agents/test_chat_history.py
@@ -2,7 +2,8 @@ import pytest
 
 from steamship import Steamship
 from steamship.agents.schema.chathistory import ChatHistory
-from steamship.data.tags.tag_constants import ChatTag
+from steamship.data.tags.tag_constants import ChatTag, TagKind
+from steamship.data.tags.tag_utils import get_tag
 
 
 @pytest.mark.usefixtures("client")
@@ -15,6 +16,9 @@ def test_chat_history_index_init(client: Steamship):
     assert chat_history.embedding_index.handle is not None
     assert chat_history.embedding_index.embedder.id is not None
 
+    # It should be marked as a Chat History
+    assert get_tag(chat_history.tags, kind=TagKind.CHAT, name=ChatTag.HISTORY)
+
     chat_history_fetched = ChatHistory.get_or_create(client, context_keys)
     assert chat_history_fetched.embedding_index is not None
     assert chat_history.embedding_index.handle == chat_history_fetched.embedding_index.handle
@@ -22,17 +26,20 @@ def test_chat_history_index_init(client: Steamship):
         chat_history.embedding_index.embedder.id == chat_history_fetched.embedding_index.embedder.id
     )
 
+    # It should be marked as a Chat History
+    assert get_tag(chat_history_fetched.tags, kind=TagKind.CHAT, name=ChatTag.HISTORY)
+
     block = chat_history.append_user_message(text="Hi. My birthday is today")
     chunk_tag = [tag for tag in block.tags if tag.name == ChatTag.CHUNK][0]
     assert chunk_tag.start_idx == 0
     assert chunk_tag.end_idx == 24
 
+    # It should be marked as a Chat Message
+    assert get_tag(block.tags, kind=TagKind.CHAT, name=ChatTag.MESSAGE)
+
     chat_history.append_user_message("I like bananas")
     chat_history.append_user_message("I also like shiny cars")
     chat_history.append_user_message("And I like programming agents")
-
-    fruit_search = chat_history.search("fruit").wait()
-    assert fruit_search.items[0].tag.text == "I like bananas"
 
     vehicle_search = chat_history.search("vehicle").wait()
     assert vehicle_search.items[0].tag.text == "I also like shiny cars"
