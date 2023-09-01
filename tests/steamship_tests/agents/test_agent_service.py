d7c6b9d256dd8e27fddde3e9c7ac3f707990c3ad
Douglas Reid <douglas-reid@users.noreply.github.com>
2023-09-01 10:09:17 -0700

    feat(tools): support non-caching tools (#541)
    When turning on an action cache for an `AgentService`, there may be
    certain tools that developers may never want to cache the results of and
    skip execution for. This PR introduces a mechanism for opting Tools out
    of caching, when desired.
    
    Co-authored-by: Douglas Reid <doug@steamship.com>

--- tests/steamship_tests/agents/test_agent_service.py
+++ tests/steamship_tests/agents/test_agent_service.py
@@ -1,11 +1,15 @@
-from typing import List
+from typing import Any, List, Union
 
 import pytest
+from pydantic.fields import PrivateAttr
 from steamship_tests import SRC_PATH
 from steamship_tests.utils.deployables import deploy_package
 
-from steamship import Block, Steamship, SteamshipError
-from steamship.agents.schema import Action, AgentContext
+from steamship import Block, Steamship, SteamshipError, Task
+from steamship.agents.functional import FunctionsBasedAgent
+from steamship.agents.llms.openai import ChatOpenAI
+from steamship.agents.schema import Action, AgentContext, Tool
+from steamship.agents.service.agent_service import AgentService
 
 
 def _blocks_from_invoke(client: Steamship, potential_blocks) -> List[Block]:
@@ -77,3 +81,74 @@ def test_example_with_caching_service(client: Steamship):
         blocks = _blocks_from_invoke(client, blocks_json)
         assert len(blocks) == 1
         assert "insert cat image here" == blocks[0].text
+
+
+class FakeUncachableTool(Tool):
+
+    name = "FakeUncacheableTool"
+    human_description = "Fake tool"
+    agent_description = "Ignored"
+
+    _runs: int = PrivateAttr(default=0)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs, cacheable=False)
+
+    @property
+    def runs(self):
+        return self._runs
+
+    def run(self, tool_input: List[Block], context: AgentContext) -> Union[List[Block], Task[Any]]:
+        self._runs += 1
+        return [Block(text="sunny and mild")]
+
+
+class FakeCachingService(AgentService):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs, use_llm_cache=True, use_action_cache=True)
+        self.set_default_agent(
+            FunctionsBasedAgent(
+                tools=[FakeUncachableTool()], llm=ChatOpenAI(self.client, temperature=0)
+            )
+        )
+
+    @property
+    def uncachable_tool(self):
+        return self.get_default_agent().tools[0]
+
+
+@pytest.mark.usefixtures("client")
+def test_non_cacheable_tool_actions(client: Steamship):
+    context_id = "foo-test-non-cacheable"
+    context_keys = {"id": context_id}
+
+    agent_context = AgentContext.get_or_create(
+        client=client, context_keys=context_keys, use_llm_cache=True, use_action_cache=True
+    )
+
+    service_under_test = FakeCachingService(client=client, config={})
+
+    # clear the cache to start fresh
+    agent_context.llm_cache.clear()
+    agent_context.action_cache.clear()
+
+    # build up cache to ensure Fake Weather Tool is called
+    fake_action = Action(
+        tool=service_under_test.uncachable_tool.name, input=[Block(text="weather in SF today")]
+    )
+    agent_context.llm_cache.update(
+        key=[Block(text="what is the weather in SF today?")], value=fake_action
+    )
+
+    fake_finish_action = Action(
+        tool="Agent-FinishAction", input=[], output=[Block(text="sunny and mild")]
+    )
+    agent_context.llm_cache.update(key=[Block(text="sunny and mild")], value=fake_finish_action)
+
+    # run once to "fill" caches
+    service_under_test.prompt(prompt="what is the weather in SF today?", context_id=context_id)
+    assert service_under_test.uncachable_tool.runs == 1
+
+    # now attempt again, and verify the Tool executed again
+    service_under_test.prompt(prompt="what is the weather in SF today?", context_id=context_id)
+    assert service_under_test.uncachable_tool.runs == 2, "The tool action was unexpectedly cached."
