9f47452f705b930f7a98eb04d37432a39be8d7ec
Ted Benson <ted@steamship.com>
2023-09-08 09:50:07 -0400

    Add ChatReplicate

--- src/steamship/agents/llms/replicate.py
+++ src/steamship/agents/llms/replicate.py
@@ -1,7 +1,8 @@
 from typing import List, Optional
 
-from steamship import Block, PluginInstance, Steamship
-from steamship.agents.schema import LLM
+from steamship import Block, File, PluginInstance, Steamship, Tag
+from steamship.agents.schema import LLM, ChatLLM, Tool
+from steamship.data import GenerationTag, TagKind
 from steamship.utils.dict_utils import remove_none
 
 PLUGIN_HANDLE = "replicate-llm"
@@ -45,12 +46,6 @@ class Replicate(LLM):
          - replicate/dolly-v2-12b
         """
 
-        # TODO: We've got whatever the opposite of an encapsulation leak here, in that this is all strongly typed
-        # and defined in the Replicate plugin, yet we can't refer to it here.
-        #
-        # One side-step approach might be to strongly type LLM configuration in the python-client repository, since
-        # it tends to be mostly stable across LLM providers, and then we can refer to it from plugins, inverting the
-        # location of definition.
         replicate_config = remove_none(
             {
                 "model_name": model_name,
@@ -82,3 +77,74 @@ class Replicate(LLM):
         action_task = self.generator.generate(text=prompt, options=options)
         action_task.wait()
         return action_task.output.blocks
+
+
+class ChatReplicate(ChatLLM, Replicate):
+    """ChatLLM that uses Steamship's Replicate plugin to generate chat completions."""
+
+    def __init__(
+        self,
+        client,
+        model_name: str = None,  # The default in the plugin is LLAMA_2_70B.
+        temperature: float = None,
+        model_version: Optional[str] = None,
+        default_system_prompt: Optional[str] = None,
+        max_tokens: Optional[int] = None,
+        top_p: Optional[float] = None,
+        stop_sequences: Optional[str] = None,  # Should be a CSV string
+        *args,
+        **kwargs
+    ):
+        """Create a new instance.
+
+        Valid model names are presently:
+         - replicate/llama-2-70b-chat
+         - replicate/dolly-v2-12b
+        """
+
+        replicate_config = remove_none(
+            {
+                "model_name": model_name,
+                "model_version": model_version,
+                "default_system_prompt": default_system_prompt,
+                "max_tokens": max_tokens,
+                "temperature": temperature,
+                "top_p": top_p,
+                "stop_sequences": stop_sequences,
+            }
+        )
+
+        replicate_config.update(**kwargs)
+        super().__init__(client=client, **replicate_config)
+
+    def chat(self, messages: List[Block], tools: Optional[List[Tool]], **kwargs) -> List[Block]:
+        """Sends chat messages to the LLM with functions from the supplied tools in a side-channel."""
+
+        options = remove_none(
+            {
+                "model_name": kwargs.get("model_name", None),
+                "model_version": kwargs.get("model_version", None),
+                "default_system_prompt": kwargs.get("default_system_prompt", None),
+                "max_tokens": kwargs.get("max_tokens", None),
+                "temperature": kwargs.get("temperature", None),
+                "top_p": kwargs.get("top_p", None),
+                "stop_sequences": kwargs.get("stop_sequences", None),
+            }
+        )
+
+        temp_file = File.create(
+            client=self.client,
+            blocks=messages,
+            tags=[Tag(kind=TagKind.GENERATION, name=GenerationTag.PROMPT_COMPLETION)],
+        )
+
+        if len(tools) > 0:
+            functions = []
+            for tool in tools:
+                functions.append(tool.as_openai_function())
+            options["functions"] = functions
+
+        tool_selection_task = self.generator.generate(input_file_id=temp_file.id, options=options)
+        tool_selection_task.wait()
+
+        return tool_selection_task.output.blocks
